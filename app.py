0c79c905069d1e3daaa8c579b852c28048271ea2
bssnehasharma <bssnehasharma@gmail.com>
2026-04-20 13:14:44 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import mysql.connector
+import plotly.exp[ress as px
+
+st.set_page_config(page_title="Ola Dashboard", layout="wide")
+st.title("Ola Rides Analysis")
